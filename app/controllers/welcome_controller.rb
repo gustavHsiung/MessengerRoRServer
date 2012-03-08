969d6496fb3bcde828c6e3fb246f8c8945623c7e
Chia-Yuan Hsiung <knightgustav@gmail.com>
2012-03-09 00:04:56 +0800

    add root welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+    def index
+    end
+
     def say
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Demo::Application.routes.draw do
+    get "welcome" => "welcome#index"
     get "welcome/say_hello" => "welcome#say"
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +51,7 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
