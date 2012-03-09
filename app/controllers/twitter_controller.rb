721ef4325798bf8e259743765efb10ab9a13b7cf
Chia-Yuan Hsiung <knightgustav@gmail.com>
2012-03-09 15:06:04 +0800

    add twitter update render

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,5 +1,7 @@
 class TwitterController < ApplicationController
     def upload
-        @photo = Photo.new(params[:media)
+        @photo = Photo.new(params[:media])
+        @filename = params[:randomFilename]
+        render :text => @filenam
     end
 end
