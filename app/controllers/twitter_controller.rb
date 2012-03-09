21bdbcb1a968af1281c0f692fc65d0cc88d9f925
Chia-Yuan Hsiung <knightgustav@gmail.com>
2012-03-09 14:54:06 +0800

    add twitter controller

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,5 @@
+class TwitterController < ApplicationController
+    def upload
+        @photo = Photo.new(params[:media)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Demo::Application.routes.draw do
+
+    root :to => 'welcome#index'
+
     get "welcome" => "welcome#index"
     get "welcome/say_hello" => "welcome#say"
     
+    post "upload" => "twitter#upload"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,10 +54,7 @@ Demo::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'welcome#index'
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
