80445e01351929486fcb3af10728652f57db728c
Chia-Yuan Hsiung <knightgustav@gmail.com>
2012-03-09 17:03:48 +0800

    - adjusted upload active

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,6 +1,5 @@
 class TwitterController < ApplicationController
     def upload
-        @photo = Photo.new(params[:media])
         @filename = params[:randomFilename]
         render :text => @filenam
     end
